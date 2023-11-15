db25c63afea9cf54446fbd4e7594c10105be461d
Denys Shyfrin <schifrindenis@gmail.com>
2023-11-15 23:12:21 +0200

    Add Base Service.

---
+++ KnowledgeBase.Client.Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,9 @@
+﻿using KnowledgeBase.Client.Web.Models.Base;
+
+namespace KnowledgeBase.Client.Web.Services.IServices
+{
+    public interface IBaseService
+    {
+        Task<ResponseDTO?> SendAsync(RequestDTO requestDto);
+    }
+}
